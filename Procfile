4a944dd8e99d62e76064232433286e3ed02a35c3
etapau <etapau@gmail.com>
2018-06-02 15:29:33 -0400

    initial configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn diag:app --log-file -
---
+++ diag.py
@@ -0,0 +1,22 @@
+import falcon
+# import rpy2's package module
+import rpy2.robjects.packages as rpackages
+
+# import R's utility package
+utils = rpackages.importr('utils')
+
+
+# Falcon follows the REST architectural style, meaning (among
+# other things) that you think in terms of resources and state
+# transitions, which map to HTTP verbs.
+class DiagResource(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = 'this is a test request'
+
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
+
+app.add_route('/test', DiagResource())
