6c17bf4636a5fe42e74f73c5826ecf444b9dfbb9
Diag <diag.davenport@gmail.com>
2018-06-12 19:26:13 -0400

    hfdstj

--- diag.py
+++ diag.py
@@ -29,11 +29,12 @@ class DiagResource(object):
             resp.body = out
         # if it doesn't work, tell the user what's wrong
         except:
-            resp.body = str(sys.exc_info())
+            #resp.body = str(sys.exc_info())
             #resp.body = 'this worked'
+            resp.body = traceback.print_exc()
         else:
             # I'll use this later
-            resp.body = 'Something went really wrong. Please email Diag at diag@uchicago.edu; he apparently messed something up.'
+            resp.body = str('Something went really wrong. Please email Diag at diag@uchicago.edu; he apparently messed something up.')
             
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
