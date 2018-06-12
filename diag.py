c9d41ba770d666559c71a7253578d255f7fbea62
Diag <diag.davenport@gmail.com>
2018-06-11 23:18:48 -0400

    late night

--- diag.py
+++ diag.py
@@ -1,8 +1,10 @@
 import falcon
 import rpy2.robjects.packages as rpackages
+import rpy2.robjects as robj
 
 # import R's utility package
 utils = rpackages.importr('utils')
+bt = rpackages.importr('blockTools')
 
 
 # Falcon follows the REST architectural style, meaning (among
@@ -12,7 +14,8 @@ class DiagResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = '1'
+        out = str(robj.r('1+2+ nchar("opp")'))
+        resp.body = out
 
 
 # falcon.API instances are callable WSGI apps
