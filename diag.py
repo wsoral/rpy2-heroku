0d390c82bd0317a40edf456fce24923f657a0b1a
Diag <diag.davenport@gmail.com>
2018-06-14 15:49:39 -0400

    Abstract the r function to the python level

--- diag.py
+++ diag.py
@@ -33,11 +33,15 @@ class DiagResource(object):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
         
-        out = robjects.r('''
-                        seqout <- seqblock(query = FALSE, id.vars = "ID", id.vals = 1, exact.vars = "Party", exact.vals = "Dem", covar.vars = "age", covar.vals = 25, file.name = "sdata.RData")
+        robjects.r('''
+                       f <- function(id, exact_var, exact_val, covar_var, covar_val, session) {
+                        seqout <- seqblock(query = FALSE, id.vars = "ID", id.vals = id, exact.vars = exact_var, exact.vals = exact_val, covar.vars = covar_var, covar.vals = covar_val, file.name = session)
                         seqout$x[seqout$x['ID'] == 1 , "Tr"]
+                        }
                        ''')
-            
+        
+        r_f = robjects.r['f']
+        out = r_f(1, "Party", "Dem", "Age", 9, "sdata.RData")
         resp.body = str(out[0])
 
 # falcon.API instances are callable WSGI apps
