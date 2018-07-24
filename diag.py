54575b3806c3312b7d2b76ce4b2492326f5e3d8a
Diag <diag.davenport@gmail.com>
2018-07-24 14:04:04 -0400

    Update diag.py

--- diag.py
+++ diag.py
@@ -26,11 +26,11 @@ class DiagResource(object):
         cap_age = req.params["age"]
         cap_party = req.params["party"]
         
-        #py_exact_var = ["gender", "education", "age", "party"]
-        #py_exact_val = [cap_gender, cap_education, cap_age, cap_party]
-        #py_session = "sdata2.RData"
+        py_exact_var = ["gender", "education", "age", "party"]
+        py_exact_val = [cap_gender, cap_education, cap_age, cap_party]
+        py_session = "sdata2.RData"
         
-        resp.body = 'test'
+        resp.body = str(cap_gender)
         
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
