32f998c052dbaa792b98438bc0fc13a3a7f29227
Diag <diag.davenport@gmail.com>
2018-07-24 14:40:39 -0400

    Update diag.py

--- diag.py
+++ diag.py
@@ -50,7 +50,7 @@ class DiagResource(object):
                            ''')
 
             r_f = robjects.r['f']
-            out = r_f(1, py_exact_var, py_exact_val, py_session)
+            out = r_f(cap_id, py_exact_var, py_exact_val, py_session)
             resp.body = 'Treatment=' + str(out[0])
         elif (req.params["party"] in ["11", "12", "13", "15", "16",  "17", "21", "23"]):
             robjects.r('''
@@ -71,7 +71,7 @@ class DiagResource(object):
                ''')
 
             r_f = robjects.r['f']
-            out = r_f(1, py_exact_var, py_exact_val, py_session)
+            out = r_f(cap_id, py_exact_var, py_exact_val, py_session)
             resp.body = 'Treatment=' + str(out[0])
         else:
             resp.body = 'Treatment=' + "fucked up" + req.params["party"]
